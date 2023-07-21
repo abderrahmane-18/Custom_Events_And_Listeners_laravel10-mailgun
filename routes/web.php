9c54add882166dc3e3c4f8c87547f8531d707b42
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2023-07-21 11:46:43 +0200

    finaly

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+use App\Mail\testingMail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('/send', function () {
+    
+    Mail::to('abdokoki087@gmail.com')->send(new testingMail());
+    return response('sending');
+   });
+//=breeze_eventn
+require __DIR__.'/auth.php';
